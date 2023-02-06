d6af1ec6c9a13a1b59c62c510435a788350cbcc0
Alhafat <vladimir-lukin@inbox.ru>
2023-02-06 20:09:32 +0300

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+int[,] GetArray()
+{
+    // System.Console.WriteLine("Введите количество строк массива: ");
+    // int m = int.Parse(Console.ReadLine()!);
+    // System.Console.WriteLine("Введите количество столбцов массива: ");
+    // int n = int.Parse(Console.ReadLine()!);
+    // int[,] array = new int[m, n];
+    int[,] array = new int[new Random().Next(0, int.MaxValue/10000),new Random().Next(0, int.MaxValue/100000)];
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(int.MinValue, int.MaxValue);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            if (numbers[i, j] >= 0) Console.Write(" " + $"{numbers[i, j]}" + "\t");
+            else Console.Write($"{numbers[i, j]}" + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumberInArray(int[,] numbers, int row, int line)
+{
+    if ((row < numbers.GetLength(0)) && (line < numbers.GetLength(1))) System.Console.WriteLine("Искомая позиция содержит следующее число: " + numbers[row, line]);
+    else System.Console.WriteLine($"Введенные параметры выходят за границы массива");
+}
+
+void Main()
+{
+    int[,] numbers = GetArray();
+    System.Console.WriteLine("Введите искомую позицию строки массива: ");
+    int row = int.Parse(Console.ReadLine()!);
+    System.Console.WriteLine("Введите искомую позицию столбца массива: ");
+    int line = int.Parse(Console.ReadLine()!);
+    // Console.WriteLine();                             //не включать
+    // PrintArray(numbers);                             //не включать
+    System.Console.WriteLine(numbers.GetLength(0));
+    System.Console.WriteLine(numbers.GetLength(1));
+    Console.WriteLine();
+    FindNumberInArray(numbers, row, line);
+}
+
+Main();
